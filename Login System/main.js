7957a4cac3a4235d0b45acfb07f63aeee86a4fd5
Thili2002 <thilinawijekoon3@gmail.com>
2025-02-24 22:57:18 +0530

    Add files via upload

---
+++ Login System/main.js
@@ -0,0 +1,7 @@
+var typingEffect = new Typed(".typedText", {
+    strings: ["Sandu", "Sandu", "Sandu"], // Corrected property name
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000, // Use a comma instead of a semicolon
+});
